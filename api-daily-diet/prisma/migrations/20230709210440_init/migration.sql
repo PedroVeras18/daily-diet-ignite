526e63bbee91de7920d9d480201232e4b5fc0a67
Pedro Veras <veraspedro1608@gmail.com>
2023-07-09 18:05:09 -0300

    feat: :sparkles: create meal table in prisma

---
+++ api-daily-diet/prisma/migrations/20230709210440_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Meal" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "isAtDiet" BOOLEAN,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Meal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
